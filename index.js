bf5b73f879a9192e88969d5894bc5ad91cb60b26
Molpe <fran.mscr@gmail.com>
2021-03-31 01:01:30 +0200

    feat:added solution

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+// Linear search solution, slow
+const linearSearch = (n, random) => {
+    let counter = BigInt(0);
+    const result = BigInt(0);
+    for(let i = -1; ++i < n; counter++) {
+        if(i === random) {
+            result = BigInt(i);
+            break;
+        }
+    }
+    return result;
+};
 
+// Binary search solution, top
 module.exports = (n, random) => {
-    return n;
+    let init = 0;
+    let end = n;
+    let chosen = -1;
+    while(chosen < random) {
+        chosen = ~~((BigInt(init) + BigInt(end)) / BigInt(2));
+        if(random > chosen) {
+            init = chosen;
+            continue;
+        }
+        if(random < chosen) {
+            end = chosen;
+        }
+    }
+    return chosen;
 };
